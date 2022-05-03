5bd5faaa0313d3ee8d98b21e5d9aad39052a1fe2
boomafromboomiland <lubomira.kovacova@avon.com>
2022-05-03 19:54:54 +0200

    fungujúce 3 info

--- covid19-zakladni-info.js
+++ covid19-zakladni-info.js
@@ -26,9 +26,20 @@ fetch('./variables.json')
             document.getElementById('js-ockovani-datum').innerText = datumOckovanych.toLocaleDateString('cs-CZ');
         
             // UKOL NA CVICENI
-            // pridej do prehledu podil pribytku nakazenych ve vekove 
+            // pridej do prehledu podil pribytku nakazenych ve vekovej skupine 65+
+            let podiel65 = ((zakladniInfo.potvrzene_pripady_65_vcerejsi_den / zakladniInfo.potvrzene_pripady_vcerejsi_den) * 100).toFixed(2);
+            document.getElementById('js-vek').innerHTML = `${podiel65}%`;
+
             // skupine 65+ za posledni mereny den z celkoveho pribytku
+            document.getElementById('js-65').innerText = parseInt(zakladniInfo.potvrzene_pripady_65_vcerejsi_den).toLocaleString('cs-CZ')
+            let datumStarsich = new Date(zakladniInfo.potvrzene_pripady_65_vcerejsi_den_datum);
+            document.getElementById('js-starsi-datum').innerText = datumStarsich.toLocaleDateString('cs-CZ');
+            
             // nakazenych za posledni mereny den
+            document.getElementById('js-nakazeny').innerText = parseInt(zakladniInfo.potvrzene_pripady_vcerejsi_den).toLocaleString('cs-CZ')
+            let datumNakazenych = new Date(zakladniInfo.potvrzene_pripady_vcerejsi_den_datum);
+            document.getElementById('js-nakazeny-datum').innerText = datumNakazenych.toLocaleDateString('cs-CZ');
+
 
             // UKOL NA CVICENI
             // proved validaci, zda plati podminka 
